6cdc5493d913e42c5de27cf7e94c632f4660635d
Pavel Gajdoš <info@pavelgajdos.cz>
2017-02-21 14:19:11 +0100

    Added FileManager control

---
+++ src/Icarus/UI/Controls/FileManager/DI/FileManagerExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Icarus\UI\Controls\DatePickers\DI;
+
+
+use Icarus\UI\Controls\FileManager\FileManager;
+use Nette\DI\CompilerExtension;
+use Nette\PhpGenerator\ClassType;
+
+
+class FileManagerExtensionsExtension extends CompilerExtension
+{
+
+    /**
+     * Adjusts DI container compiled to PHP class. Intended to be overridden by descendant.
+     *
+     * @param \Nette\PhpGenerator\ClassType $class class, interface, trait description
+     * @return void
+     */
+    public function afterCompile(ClassType $class)
+    {
+        parent::afterCompile($class);
+
+        $initialize = $class->methods['initialize'];
+        $initialize->addBody(FileManager::class . '::register');
+    }
+}
